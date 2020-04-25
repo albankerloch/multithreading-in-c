256842951788973d600cc991217aa7be3f77cfb4
albankerloch <alban.kerloch@gmail.com>
2020-04-25 23:25:59 +0200

    test vm

--- philo_one/main.c
+++ philo_one/main.c
@@ -3,10 +3,19 @@
 #include <pthread.h>
 #include <string.h> 
 #include <unistd.h>
- 
+#include <sys/time.h>
+
 #define INITIAL_STOCK   20
 #define NB_CLIENTS      5
- 
+
+long long current_timestamp()
+{
+    struct timeval te; 
+    gettimeofday(&te, NULL);
+    long long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000;
+    return milliseconds;
+}
+
 typedef struct
 {
    int stock;
@@ -24,19 +33,23 @@ static void * fn_philo (void * p_data)
   return NULL;
 }
  
-int main (void)
+int main (int ac, char **av)
 {
    int ret;
    void *t;
    pthread_t thread_philo;
+
+   (void)ac;
+   (void)av;
    
    ret = 0;
-   printf ("Creation du thread du philosophe !\n");
+   write(1, "Creation\n", 9);
    ret = pthread_create(&thread_philo, NULL, fn_philo, NULL);
  
    if (!ret)
    {
-      printf ("Creation des threads clients !\n");
+     printf("%lld \n", current_timestamp());
+     write(1, "1 is thinking\n", 15);
    }
    else
    {
