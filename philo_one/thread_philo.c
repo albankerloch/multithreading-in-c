43f4a76d735d74606c0755b4c6ff00194cb859a3
albankerloch <alban.kerloch@gmail.com>
2020-05-02 02:47:11 +0200

    ft_message

--- philo_one/thread_philo.c
+++ philo_one/thread_philo.c
@@ -1,5 +1,15 @@
 #include "philo_one.h"
 
+void	ft_message(node *n, char *str, long long tm)
+{
+	pthread_mutex_lock(n->lock_s);
+	ft_putlnbr_fd(tm, 1);
+	write(1, " ", 1);
+	ft_putnbr_fd(n->value, 1);
+	write(1, str, ft_strlen(str));
+	pthread_mutex_unlock(n->lock_s);
+}
+
 void	*fn_philo(void *p_data)
 {
 	node *n;
@@ -9,12 +19,7 @@ void	*fn_philo(void *p_data)
 	if (pthread_create(&(n->monitor_die), NULL, fn_monitor, p_data))
 		return (0);
 	pthread_detach(n->monitor_die);
-	pthread_mutex_lock(n->lock_s);
-	ft_putlnbr_fd(n->start, 1);
-	write(1, " ", 1);
-	ft_putnbr_fd(n->value, 1);
-	write(1, " is thinking\n", 14);
-	pthread_mutex_unlock(n->lock_s);
+	ft_message(n, " is thinking\n", n->start);
 	while (1)
 	{
 		while (!(n->fork_lock == 0 && n->next->fork_lock == 0))
@@ -25,27 +30,11 @@ void	*fn_philo(void *p_data)
 		n->fork_lock = 1;
 		pthread_mutex_lock(&(n->next->lock));
 		n->next->fork_lock = 1;
-		pthread_mutex_lock(n->lock_s);
-		ft_putlnbr_fd(current_timestamp(), 1);
-		write(1, " ", 1);
-		ft_putnbr_fd(n->value, 1);
-		ft_putnbr_fd(n->value, 1);
-		write(1, " has taken a fork\n", 18);
-		ft_putlnbr_fd(current_timestamp(), 1);
-		write(1, " ", 1);
-		ft_putnbr_fd(n->value, 1);
-		ft_putnbr_fd(n->next->value, 1);
-		write(1, " has taken a fork\n", 18);
-		pthread_mutex_unlock(n->lock_s);
+		ft_message(n, " has taken a fork\n", current_timestamp());
+		ft_message(n, " has taken a fork\n", current_timestamp());
 		n->start = current_timestamp();
 		n->count_eat = n->count_eat + 1;
-		pthread_mutex_lock(n->lock_s);
-		ft_putlnbr_fd(n->start, 1);
-		write(1, " ", 1);
-		ft_putnbr_fd(n->value, 1);
-		ft_putnbr_fd(n->count_eat, 1);
-		write(1, " is eating\n", 11);
-		pthread_mutex_unlock(n->lock_s);
+		ft_message(n, " is eating\n", n->start);
 		usleep(n->tt_eat * 1000);
 		n->next->fork_lock = 0;
 		pthread_mutex_unlock(&(n->next->lock));
@@ -56,19 +45,9 @@ void	*fn_philo(void *p_data)
 			{
 				usleep(5000 * 1000);
 			}
-		pthread_mutex_lock(n->lock_s);
-		ft_putlnbr_fd(current_timestamp(), 1);
-		write(1, " ", 1);
-		ft_putnbr_fd(n->value, 1);
-		write(1, " is sleeping\n", 13);
-		pthread_mutex_unlock(n->lock_s);
+		ft_message(n, " is sleeping\n", current_timestamp());
 		usleep(n->tt_sleep * 1000);
-		pthread_mutex_lock(n->lock_s);
-		ft_putlnbr_fd(current_timestamp(), 1);
-		write(1, " ", 1);
-		ft_putnbr_fd(n->value, 1);
-		write(1, " is thinking\n", 14);
-		pthread_mutex_unlock(n->lock_s);
+		ft_message(n, " is thinking\n", current_timestamp());
 	}
 	return (0);
 }
