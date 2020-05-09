72908b80297e32c8f78a94014c2d7fa6bf133e56
albankerloch <alban.kerloch@gmail.com>
2020-05-09 20:01:13 +0200

    replace semaphore

--- philo_two/thread_philo.c
+++ philo_two/thread_philo.c
@@ -28,20 +28,14 @@ void		ft_message(node *n, char *str, long long tm, unsigned int j)
 
 static void	ft_activity(node *n)
 {
-	pthread_mutex_lock(&(n->lock));
-	n->fork_lock = 1;
-	pthread_mutex_lock(&(n->next->lock));
-	n->next->fork_lock = 1;
 	ft_message(n, " has taken a fork\n", current_timestamp(), 17);
 	ft_message(n, " has taken a fork\n", current_timestamp(), 17);
 	n->start = current_timestamp();
 	n->count_eat = n->count_eat + 1;
 	ft_message(n, " is eating\n", n->start, 10);
 	usleep(n->tt_eat * 1000);
-	n->next->fork_lock = 0;
-	pthread_mutex_unlock(&(n->next->lock));
-	n->fork_lock = 0;
-	pthread_mutex_unlock(&(n->lock));
+	sem_post(n->var->sem_fork);
+	sem_post(n->var->sem_fork);
 	if (n->count_eat == n->nb_eat)
 		while (1)
 		{
@@ -64,10 +58,8 @@ void		*fn_philo(void *p_data)
 	ft_message(n, " is thinking\n", n->start, 12);
 	while (1)
 	{
-		while (!(n->fork_lock == 0 && n->next->fork_lock == 0))
-		{
-			usleep(10);
-		}
+		sem_wait(n->var->sem_fork);
+		sem_wait(n->var->sem_fork);
 		ft_activity(n);
 	}
 	return (0);
