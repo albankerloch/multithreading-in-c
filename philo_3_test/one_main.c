0a61a11b035afaae7940126f071f910e39000f39
albankerloch <alban.kerloch@gmail.com>
2020-05-19 00:04:35 +0200

    fork en gardant structure et monitor die

---
+++ philo_3_test/one_main.c
@@ -0,0 +1,121 @@
+#include "philo_one.h"
+
+long long	current_timestamp(void)
+{
+	struct timeval	te;
+	long long		m;
+
+	gettimeofday(&te, NULL);
+	m = te.tv_sec * 1000LL + te.tv_usec / 1000;
+	return (m);
+}
+
+void		*fn_monitor_eat(void *p_data)
+{
+	node	*n;
+	int		nb_eat_total;
+	int		i;
+
+	n = p_data;
+	while (1)
+	{
+		usleep(10 * 1000);
+		nb_eat_total = 0;
+		i = 1;
+		while (i < n->nb + 1)
+		{
+			nb_eat_total = nb_eat_total + n->count_eat;
+			n = n->next;
+			i++;
+		}
+		if (nb_eat_total == n->nb * n->nb_eat)
+		{
+			sem_post(n->sem_die);
+			return (0);
+		}
+	}
+	return (0);
+}
+
+void		*fn_monitor(void *p_data)
+{
+	node *n;
+
+	n = p_data;
+	while (1)
+	{
+		usleep(10 * 000);
+		if (current_timestamp() - n->start > n->tt_die && \
+n->count_eat != n->nb_eat)
+		{
+			ft_message(n, " died\n", n->start, 5);
+			sem_post(n->sem_die);
+			break;
+		}
+	}
+	return (0);
+}
+
+static int
+	routine(node *n)
+{
+	void *p;
+
+	p = (void *)n;
+	n->start = current_timestamp();
+	if (pthread_create(&(n->monitor_die), NULL, fn_monitor, p))
+		return (0);
+	pthread_detach(n->monitor_die);
+	ft_message(n, " is thinking\n", n->start, 12);
+	while (1)
+	{
+		sem_wait(n->var->sem_fork);
+		sem_wait(n->var->sem_fork);
+		ft_activity(n);
+	}
+	return (0);
+}
+
+int			main(int ac, char **av)
+{
+	int		i;
+	bin		var;
+//	void	*t;
+
+	if (ft_arg(&var, ac, av))
+		return (1);
+
+	var.str_fork[0] = 'f';
+	var.str_fork[1] = '\0';
+	sem_unlink(var.str_fork);
+	var.sem_fork = sem_open(var.str_fork, O_CREAT | O_EXCL, 0664, var.nb);
+	var.str_die[0] = 'd';
+	var.str_die[1] = '\0';
+	sem_unlink(var.str_die);
+	var.sem_die = sem_open(var.str_die, O_CREAT | O_EXCL, 0664, 1);
+	sem_wait(var.sem_die);
+
+	if (ft_create(&var))
+		return (1);
+
+	i = 1;
+	while (i < var.nb + 1)
+	{
+		var.philo[i].pid = fork();
+		if (var.philo[i].pid < 0)
+			return (1);
+		else if (var.philo[i].pid == 0)
+		{
+			routine(&var.philo[i]);
+			exit(0);
+		}
+		usleep(100);
+		printf("[%d] [%d] hi i=%d fork=%d\n", getppid(), getpid(), i, var.philo[i].pid);
+		i++;
+	}
+	sem_wait(var.sem_die);
+	i = 1;
+	while (i < var.nb + 1)
+		kill(var.philo[i++].pid, SIGKILL);
+	return (ft_clear(&var, var.nb));
+}
---
+++ philo_3_test/thread_philo.c
@@ -0,0 +1,67 @@
+#include "philo_one.h"
+
+void		ft_chg_str(node *n, char *str, unsigned int t, unsigned int j)
+{
+	unsigned int i;
+	
+	i = 0;
+	while (i < j)
+	{
+		n->str[i + t] = str[i];
+		i++;
+	}
+	n->str[i + t ] = '\n';
+	n->str[i + t + 1] = '\0';
+}
+
+void		ft_message(node *n, char *str, long long tm, unsigned int j)
+{
+	unsigned int t;
+
+	ft_putlnbr_str(tm, n);
+	n->str[13] = ' ';
+	ft_putnbr_str(n->value, n);
+	t = ft_strlen(n->str);
+	n->str[t] = ' ';
+	ft_chg_str(n, str, t, j);
+	write(1, n->str, ft_strlen(n->str));
+}
+
+void	ft_activity(node *n)
+{
+	ft_message(n, " has taken a fork\n", current_timestamp(), 17);
+	ft_message(n, " has taken a fork\n", current_timestamp(), 17);
+	n->start = current_timestamp();
+	n->count_eat = n->count_eat + 1;
+	ft_message(n, " is eating\n", n->start, 10);
+	usleep(n->tt_eat * 1000);
+	sem_post(n->var->sem_fork);
+	sem_post(n->var->sem_fork);
+	if (n->count_eat == n->nb_eat)
+		while (1)
+		{
+			usleep(5000 * 1000);
+		}
+	ft_message(n, " is sleeping\n", current_timestamp(), 12);
+	usleep(n->tt_sleep * 1000);
+	ft_message(n, " is thinking\n", current_timestamp(), 12);
+}
+
+void		*fn_philo(void *p_data)
+{
+	node *n;
+
+	n = p_data;
+	n->start = current_timestamp();
+	if (pthread_create(&(n->monitor_die), NULL, fn_monitor, p_data))
+		return (0);
+	pthread_detach(n->monitor_die);
+	ft_message(n, " is thinking\n", n->start, 12);
+	while (1)
+	{
+		sem_wait(n->var->sem_fork);
+		sem_wait(n->var->sem_fork);
+		ft_activity(n);
+	}
+	return (0);
+}
